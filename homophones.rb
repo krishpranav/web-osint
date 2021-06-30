b1e6972096f33f399d13187c6f147a2fbb79e9e6
krishpranav <krisna.pranav@gmail.com>
2021-06-30 11:24:04 +0530

    [+] dictionary [+]

--- homophones.rb
+++ homophones.rb
@@ -1,5 +1,12 @@
 class Homophones
   @@homophones=File.readlines($LOAD_PATH.first + "/homophones.txt").delete_if { |line| line =~ /^#/ or line =~ /^[ ]*$/ }.join
 
+
+  def self.dictionary
+    setup unless defined?  @@dictionary
+    @@dictionary
+  end
+
+  private
   
 end
