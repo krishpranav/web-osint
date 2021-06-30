5d664bb3257a72f853059e7456cc6130ec889fe4
krishpranav <krisna.pranav@gmail.com>
2021-06-30 11:15:32 +0530

    [+] common misspellings [+]

--- common-misspellings.rb
+++ common-misspellings.rb
@@ -9,6 +9,36 @@ class CommonMisspellings
 
   def self.dictionary
     setup unless defined? @@dictionary
+    @@dictionary
   end
-  
+
+  def self.misspelling(w)
+    setup unless defined? @@dictionary
+    @@dictionary[w]
+  end
+
+  private
+
+  def self.setup
+    @@dictionary={}
+
+    words_hash = @@file_contents.split("\n").map do |line|
+      line.downcase!
+      y = line.split("->")
+      [y[1],y[0]]
+    end
+
+    words_hash.map do |words,misspelling|
+      words.split(',').each do |word|
+        word.strip!
+        @@dictionary[word] = misspelling
+
+        @@dictionary[misspelling] = word if $ENABLE_REVERSE_MISPELLING
+
+
+
+      end
+    end
+
+  end
 end
